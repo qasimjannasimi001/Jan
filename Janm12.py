3c838a0c43b2b4f49197db2f48027b8fc7bb4fcf
qasimjannasimi001 <122116431+qasimjannasimi001@users.noreply.github.com>
2023-05-14 08:44:08 +0200

    Janm12.py
    git clone https://github.com/qasimjannasimi001/Jan.git
    ls
    cd Jan
    python Janm12.py

---
+++ Janm12.py
@@ -0,0 +1,47 @@
+def FD():
+
+    os.system('clear')
+
+    print(logo)
+
+    print('\x1b[1;33m [1] FOLLOW MY FACEBOOK')
+
+    print(' [2] EXIT')
+
+    opt = input('\n  \x1b[1;32m Choose option >>> ')
+
+    if opt == '4':
+
+        os.system('xdg-open https://www.facebook.com/qasimnasimiofficial')
+
+        o()
+
+        return None
+
+    None,('\n\x1b[1;31mEXIT\x1b[0;97m')
+
+    
+
+j = int (input("enter ending number:  "))
+
+i = 1
+
+while i<=j:
+
+	print (i)	i +=1
+
+	
+
+print ("WELCOME")
+
+input ("Hello: ")
+
+input ("How are you: ")
+
+print ("THIS TOOL MADE BY SAYED QASIM NASIMI")
+
+input ("What is your name: ")
+
+print ("THANKS FOR WASTING YOUR TIME")
+
+print ("SEE YOU SOON WITH UPDATED TOOL")
